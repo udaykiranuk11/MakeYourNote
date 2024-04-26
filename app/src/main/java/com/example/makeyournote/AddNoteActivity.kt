c344aad0c4d357c9215f459ab4b0ab61b88607e6
UDAY KIRAN T <udaykirant211@gmail.com>
2024-04-26 13:36:07 +0530

    initial commit
    it is my first project using kotlin in android

---
+++ app/src/main/java/com/example/makeyournote/AddNoteActivity.kt
@@ -0,0 +1,33 @@
+package com.example.makeyournote
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.makeyournote.databinding.ActivityAddNoteBinding
+
+class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/makeyournote/Note.kt
@@ -0,0 +1,4 @@
+package com.example.makeyournote
+
+data class Note(val id: Int, val title: String, val content: String)
+
